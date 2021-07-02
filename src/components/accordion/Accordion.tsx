af6f1ad9c2c0c1614b371f9d170c5660bfa40a19
IliaD <id@diler.ru>
2021-07-02 10:57:53 +0300

    add "new project ToDoist"

--- src/components/accordion/Accordion.tsx
+++
@@ -1,8 +0,0 @@
-import './Accordion.css';
-import { AccordionItem } from "../../interfaces/Accordion.interface";
-import { AccordionElement } from "./AccordionElement";
-
-export const Accordion = ( { ...props } ): JSX.Element => {
-  const { data } = props;
-  return data.map( ( value: AccordionItem, index: number ) => <AccordionElement value={ value } key={index}/> );
-}
---
+++ src/components/layers/container/Container.tsx
@@ -0,0 +1,7 @@
+import React, { FC, PropsWithChildren, ReactNode } from "react";
+
+export const Container: FC = ( {children} ) => {
+	return (<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+		{ children }
+	</div>);
+};
